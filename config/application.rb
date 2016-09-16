d5b6a2a229aeca9a12c2067cb377902304503cfa
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-09-16 23:11:00 +0900

    refs #1 slimに変換

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,14 @@ module Mrennai
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    # template generators's configuration
+    config.sass.preferred_syntax :sass
+    config.generators do |g|
+      g.template_engine :slim
+      g.test_framework :rspec, view_specs: false, helper_space: false
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+    end
   end
 end
