5d5faf6372f67acd5b7fde22b61ae4a9290af9a3
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-09-19 13:35:50 +0900

    修正

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,7 +4,7 @@ class UserSessionsController < ApplicationController
   end
 
   def create
-    user = login(params[:email, params[:password]])
+    user = login(params[:email], params[:password])
     if user
       redirect_back_or_to(:users, notice: 'ログインに成功しました')
     else
