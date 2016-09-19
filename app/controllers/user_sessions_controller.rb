dd19dcc531b813fbc5f76d9a77edac93e8488475
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-09-19 13:30:19 +0900

    sessionを作成

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,20 @@
+class UserSessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = login(params[:email, params[:password]])
+    if user
+      redirect_back_or_to(:users, notice: 'ログインに成功しました')
+    else
+      flash.now[:alert] = 'ログインに失敗しました'
+      render 'new'
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to(:users, notice: 'ログアウト')
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'user_sessions/new'
-
-  get 'user_sessions/create'
-
-  get 'user_sessions/destroy'
-
-  resources :users
   root 'top#index'
+  resources :user_sessions
+  resources :users
+
+  get 'login' => 'user_sessions#new', as: :login
+  get 'logout' => 'user_sessions#destroy', as: :logout
 end
