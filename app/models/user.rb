103ee082cd004c2fec8a16f40092f5e4391fbbb5
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-09-19 11:18:41 +0900

    Userモデルのログイン準備

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+  authenticates_with_sorcery!
+
+  validates :password, length: { minimum: 8 },
+                       confirmation: true
+  validates :password_confirmation, presence: true
+
+  validates :email, uniqueness: true
 end
