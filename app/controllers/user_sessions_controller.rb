922f61860c9d03bdb8bcc6579af7b0f5a55902f9
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-09-23 23:37:36 +0900

    I18nに変更 レイアウトをone columnに変更

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,5 +1,7 @@
 class UserSessionsController < ApplicationController
+  layout 'one_column'
   skip_before_action :require_login, except: :destroy
+
   def new
     @user = User.new
   end
@@ -7,15 +9,15 @@ class UserSessionsController < ApplicationController
   def create
     user = login(params[:email], params[:password])
     if user
-      redirect_back_or_to(:users, notice: 'ログインに成功しました')
+      redirect_back_or_to(:users, notice: I18n.t('login_was_successfully_created'))
     else
-      flash.now[:alert] = 'ログインに失敗しました'
+      flash.now[:alert] = I18n.t('login_was_failed')
       render 'new'
     end
   end
 
   def destroy
     logout
-    redirect_to(:users, notice: 'ログアウト')
+    redirect_to(:users, notice: I18n.t('login_was_successfully_deleted'))
   end
 end
