ce1a3a789f420dda1919f8f870fb747a69f4dcf2
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-09-23 22:31:40 +0900

    %i記法に変更 不要なファイルを削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   layout 'one_column'
   skip_before_action :require_login, only: %i(new create)
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: %i(show edit update destroy)
 
   def index
     @users = User.all
