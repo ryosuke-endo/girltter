21fa23fba52c3490491ad202a0a7f38e9866d28c
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-09-24 14:32:58 +0900

    sub_navをカテゴリーを出すように変更

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,13 @@
 class TopController < ApplicationController
   skip_before_action :require_login
+  before_action :set_category
 
   def index
+  end
+
+  private
+
+  def set_category
+    @love_categories = LoveCategory.all
   end
 end
