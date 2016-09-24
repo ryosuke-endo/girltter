40cf8c804f047ff093890b3b3f7530d74ee66606
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-09-24 13:35:40 +0900

    カテゴリーモデルを作成する

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160924040832) do
+ActiveRecord::Schema.define(version: 20160924043154) do
 
-  create_table "loves", force: :cascade do |t|
-    t.string   "title",      limit: 255,   null: false
-    t.text     "body",       limit: 65535, null: false
-    t.integer  "user_id",    limit: 4,     null: false
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",       limit: 255, null: false
+    t.string   "type",       limit: 255, null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
+  create_table "loves", force: :cascade do |t|
+    t.string   "title",       limit: 255,   null: false
+    t.text     "body",        limit: 65535, null: false
+    t.integer  "user_id",     limit: 4,     null: false
+    t.integer  "category_id", limit: 4,     null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
+
+  add_index "loves", ["category_id"], name: "index_loves_on_category_id", using: :btree
   add_index "loves", ["user_id"], name: "index_loves_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
