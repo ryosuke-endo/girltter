d4d8dc32af21ce086460b161d6505c290e342c0c
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-09-25 16:49:40 +0900

    createアクションを作成

--- app/controllers/loves_controller.rb
+++ app/controllers/loves_controller.rb
@@ -1,7 +1,7 @@
 class LovesController < ApplicationController
   skip_before_action :require_login, only: %i(index show)
-  before_action :set_categories
-  before_action :set_loves
+  before_action :set_categories, only: %i(index new)
+  before_action :set_loves, only: :index
 
   def index
   end
@@ -11,8 +11,16 @@ class LovesController < ApplicationController
   end
 
   def new
-    @love_categories = LoveCategory.all.pluck(:name, :id)
     @love = Love.new
+  end
+
+  def create
+    @love = current_user.loves.build(love_params)
+    if @love.save
+      redirect_to @love, notice: t('love_was_successfully_created')
+    else
+      render :new
+    end
   end
 
   private
@@ -23,5 +31,11 @@ class LovesController < ApplicationController
 
   def set_loves
     @loves = Love.order(created_at: :desc).page(params[:page])
+  end
+
+  def love_params
+    params.require(:love).permit(:category_id,
+                                 :body,
+                                 :title)
   end
 end
