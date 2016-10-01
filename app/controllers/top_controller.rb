250726bc60097a6e6968cb62ba1c660d0b1fad0e
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-10-02 00:08:19 +0900

    refs トップページに投稿されている名言を載せる #11 トップページの修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,11 +1,16 @@
 class TopController < ApplicationController
   skip_before_action :require_login
   before_action :set_categories
+  before_action :set_loves
 
   def index
   end
 
   private
+
+  def set_loves
+    @loves = Love.order(created_at: :desc).page(params[:page])
+  end
 
   def set_categories
     @categories = LoveCategory.all
