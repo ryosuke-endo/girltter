936ea6071bda4ff0a00a8f302edc8a6f4556df91
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-10-03 23:05:13 +0900

    テストを追加

--- spec/models/love_spec.rb
+++ spec/models/love_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Love, type: :model do
-  let(:love) { build(:love) }
+  let(:love) { build(:love, :with_member) }
+  let!(:tag) { create(:tag) }
 
   %w(title body).each do |column|
     describe "#{column}" do
@@ -9,6 +10,14 @@ RSpec.describe Love, type: :model do
         love[column] = ''
         expect(love).not_to be_valid
       end
+    end
+  end
+
+  describe 'tags' do
+    it 'tagsの言葉が本文にある場合は保存される' do
+      love.body = "脈ありですか？"
+      love.save
+      expect(love.tag_list).to include(tag.name)
     end
   end
 end
