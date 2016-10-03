6a7fe00ab27061730eb52ffa974f5788497f3dff
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-10-04 08:02:03 +0900

    カラムを変更

--- db/migrate/20160924040832_create_loves.rb
+++ db/migrate/20160924040832_create_loves.rb
@@ -3,7 +3,7 @@ class CreateLoves < ActiveRecord::Migration
     create_table :loves do |t|
       t.string :title, null: false
       t.text :body, null: false
-      t.references :user, index: true, foreign_key: true, null: false
+      t.integer :user, index: true, null: false
 
       t.timestamps null: false
     end
