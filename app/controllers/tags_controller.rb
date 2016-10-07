ccb594185c8ece106aecef29be9b3ddb160ff7b0
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-10-07 23:30:10 +0900

    paginateに対応

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,15 +1,26 @@
 class TagsController < ApplicationController
+  before_action :set_tag
+
   def show
-    @threads = []
+    @threads = Kaminari.paginate_array(threads).page(params[:page])
+  end
+
+  private
+
+  def set_tag
     @tag = ActsAsTaggableOn::Tag.find(params[:id])
+  end
+
+  def threads
+    threads = []
     tags_type = Tagging.where(tag_id: @tag.id)
                        .pluck(:taggable_type).uniq
     tags_type.each do |type|
       case type
       when 'Love'
-        @threads << Love.tagged_with(@tag.name)
+        threads << Love.tagged_with(@tag.name)
       end
     end
-    @threads.flatten!
+    threads.flatten!
   end
 end
