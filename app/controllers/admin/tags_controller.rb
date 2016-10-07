089760729217269192855fa1c12c3c4029c5251f
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-10-07 13:46:16 +0900

    tag controllerの削除メソッド作成

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -12,11 +12,23 @@ class Admin::TagsController < AdminController
   end
 
   def create
+    @tag = ActsAsTaggableOn::Tag.new(name: params[:name])
+    if @tag.save
+      redirect_to admin_tags_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    tag = ActsAsTaggableOn::Tag.find(params[:id])
+    tag.destroy
+    redirect_to admin_tags_path, notice: t('tag_was_successfully_deleted')
   end
 
   private
 
   def set_tags
-    @tags = ActsAsTaggableOn::Tag.all
+    @tags = ActsAsTaggableOn::Tag.page(params[:page]).per(25)
   end
 end
