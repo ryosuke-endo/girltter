f6cd26e1253603d03196797ff8fc747d2ab78746
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-10-08 08:50:50 +0900

    テストを追加

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    let(:comment) { build(:comment) }
+    context 'body' do
+      it '存在しないといけない' do
+        comment.body = ''
+        expect(comment).not_to be_valid
+      end
+    end
+  end
 end
