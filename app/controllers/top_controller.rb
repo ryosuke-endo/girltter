08562593fa99e05aaadee018944b75a8762b0d74
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-10-07 16:43:10 +0900

    refs タグランキングの設置

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,6 +2,7 @@ class TopController < ApplicationController
   skip_before_action :require_login
   before_action :set_categories
   before_action :set_loves
+  before_action :set_tag_ranking
 
   def index
   end
@@ -15,5 +16,10 @@ class TopController < ApplicationController
 
   def set_categories
     @categories = LoveCategory.all
+  end
+
+  def set_tag_ranking
+    @tag_ranking = ActsAsTaggableOn::Tag.select(:name)
+      .order(taggings_count: :desc).limit(20)
   end
 end
