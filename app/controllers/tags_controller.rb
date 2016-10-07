4a4a6257ca5c19a01d135477db46ceea763e8f82
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-10-07 23:05:39 +0900

    tagの一覧ページを作成

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,14 @@
+class TagsController < ApplicationController
+  def show
+    @threads = []
+    @tag = ActsAsTaggableOn::Tag.find(params[:id])
+    tags_type = Tagging.where(tag_id: @tag.id)
+                       .pluck(:taggable_type).uniq
+    tags_type.each do |type|
+      case type
+      when 'Love'
+        @threads << Love.tagged_with(@tag.name)
+      end
+    end
+  end
+end
