d96a18501262e2bcfca17ad0f391e4ec9a3e8144
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-10-07 15:23:22 +0900

    タグをモデルに関連づける

--- app/controllers/admin/tags/associate_with_model_controller.rb
+++
@@ -1,7 +0,0 @@
-class Admin::Tags::AssociateWithModelController < ApplicationController
-  def new
-  end
-
-  def create
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,8 @@ Rails.application.routes.draw do
   resources :admin, only: :index
   namespace :admin do
     resources :tags
+    namespace :tags do
+      resource :associate_with_model, only: %i(new create)
+    end
   end
 end
