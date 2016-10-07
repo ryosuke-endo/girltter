a36d6783e4199a0d3c26526025302069ef30e72a
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-10-07 14:15:55 +0900

    update_tags!メソッドを追加

--- app/models/love.rb
+++ app/models/love.rb
@@ -9,22 +9,31 @@ class Love < ActiveRecord::Base
   validates :category_id, presence: true
   validates :title, presence: true
 
+  class << self
+    def update_tags!
+      tags = ActsAsTaggableOn::Tag.pluck(:name)
+      ActiveRecord::Base.transaction do
+        Love.find_each do |love|
+          tags.each do |tag|
+            if !!(love.body.match(tag))
+              love.tag_list << tag
+            end
+          end
+          if love.tag_list.present?
+            love.save!
+          end
+        end
+      end
+    end
+  end
+
+  private
+
   def set_tags
     tags = ActsAsTaggableOn::Tag.pluck(:name)
     tags.each do |tag|
       if !!(body.match(tag))
         tag_list << tag
-      end
-    end
-  end
-
-  class << self
-    def update_tags!
-      tags = ActsAsTaggableOn::Tag.pluck(:name)
-      tags.each do |tag|
-        if !!(body.match(tag))
-          tag_list << tag
-        end
       end
     end
   end
