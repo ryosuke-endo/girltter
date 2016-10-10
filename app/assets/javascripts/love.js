3f472613e93c94bb30ec868d7f52886fe9f5728e
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-10-10 18:14:40 +0900

    classっぽくする

--- app/assets/javascripts/love.js
+++ app/assets/javascripts/love.js
@@ -1,23 +1,38 @@
 document.addEventListener("turbolinks:load", () => {
-  let elem = document.querySelector("[data-form-submit-comment]");
-  elem.addEventListener('click', (event) => {
+  class Comment {
+    constructor(comments) {
+      this.target_id = comments.childElementCount;
+      this.body = document.querySelector("[data-form-text-area-comment]").value;
+      this.commentable_id = document.getElementById('comment_commentable_id').value;
+      this.commentable_type = document.getElementById('comment_commentable_type').value;
+      this.member_id = document.getElementById('comment_member_id').value;
+    }
+
+    send() {
+      $.ajax({
+        url: "/comments",
+        type: "POST",
+        data: { comment: { body: this.body,
+                           commentable_id: this.commentable_id,
+                           commentable_type: this.commentable_type,
+                           member_id: this.member_id } },
+        success: (data) => {
+          $(`[data-comment-no=${this.target_id}]`).after(data);
+        }
+      });
+    }
+
+    static form() {
+      return document.querySelector("[data-form-submit-comment]");
+    }
+
+    static comments() {
+      return document.querySelector("[data-comments]");
+    }
+  }
+  Comment.form().addEventListener('click', (event) => {
     event.preventDefault();
-    let comments = document.querySelector("[data-comments]")
-    let target_id  = comments.childElementCount
-    let body = document.querySelector("[data-form-text-area-comment]").value;
-    let commentable_id = document.getElementById('comment_commentable_id').value
-    let commentable_type = document.getElementById('comment_commentable_type').value
-    let member_id = document.getElementById('comment_member_id').value
-    $.ajax({
-      url: "/comments",
-      type: "POST",
-      data: { comment: { body: body,
-                         commentable_id: commentable_id,
-                         commentable_type: commentable_type,
-                         member_id: member_id } },
-      success: (data) => {
-        $(`[data-comment-no=${target_id}]`).after(data)
-      }
-    });
-  });
+    comment = new Comment(Comment.comments());
+    comment.send();
+  })
 });
