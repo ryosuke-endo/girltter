fa02ad502eba1ef4b3550df6d86d64dfdc10aca3
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-10-16 22:20:16 +0900

    並び順を変更

--- app/controllers/loves_controller.rb
+++ app/controllers/loves_controller.rb
@@ -42,7 +42,6 @@ class LovesController < ApplicationController
   def set_love
     @love = Love.find(params[:id])
     @comments = @love.comments.includes(:member)
-      .order(created_at: :desc)
   end
 
   def set_loves
