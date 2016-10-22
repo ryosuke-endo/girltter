2d496e3a1bc0c863235d6c2c38a188274520946f
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-10-22 17:48:06 +0900

    modal表示とコメント文を消すように変更

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -17,6 +17,8 @@ class Comment {
                          member_id: this.member_id } }
     }).done((data) => {
       this.target_id.after(data);
+      deleteComment();
+      modalOpen();
     }).fail((data) => {
       console.log("fail");
     });
@@ -35,6 +37,10 @@ function getComment() {
   return document.querySelector("[data-form-text-area-comment]").value;
 }
 
+function deleteComment() {
+  return document.querySelector("[data-form-text-area-comment]").value = "";
+}
+
 function getTargetID(comments) {
   if (comments.childElementCount === 0){
     return $(`[data-comments]`)
@@ -44,6 +50,24 @@ function getTargetID(comments) {
   }
 }
 
+function modalOpen(){
+  centaringModal();
+  $("#c-modal__content").fadeIn("slow");
+  setTimeout(() => {
+    $("#c-modal__content").fadeOut("slow",() => {
+      $("#c-modal__content").remove();
+    });
+  },3000);
+}
+
+function centaringModal() {
+  let w = $(window).width();
+  let h = $(window).height();
+  let cw = $("#c-modal__content").outerWidth();
+  let ch = $("#c-modal__content").outerHeight();
+  $("#c-modal__content").css({"left": ((w - cw)/2) + "px","top": ((h - ch)/2)+ "px"});
+}
+
 commentSubmit().addEventListener('click', (event) => {
   if (getComment()) {
     comment = new Comment(getComments());
@@ -51,25 +75,3 @@ commentSubmit().addEventListener('click', (event) => {
     comment.send();
   }
 });
-
-$(function(){
-  $("#modal-open").click(function() {
-    centaringModal();
-    $("#c-modal__content").fadeIn("slow");
-    // setTimeout(function(){
-    //   $("#c-modal__content").fadeOut("slow",function(){
-    //     $("#c-modal__content").remove();
-    //   });
-    // },3000);
-  });
-});
-
-function centaringModal() {
-  let w = $(window).width();
-  let h = $(window).height();
-  // let cw = $("#c-modal__content").outerWidth({margin:true});
-  // let ch = $("#c-modal__content").outerHeight({margin:true});
-  let cw = $("#c-modal__content").outerWidth();
-  let ch = $("#c-modal__content").outerHeight();
-  $("#c-modal__content").css({"left": ((w - cw)/2) + "px","top": ((h - ch)/2)+ "px"});
-}
