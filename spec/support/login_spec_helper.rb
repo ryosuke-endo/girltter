99a63f324553c3750d10ac310735894633dcadce
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-10-23 12:09:45 +0900

    不要なスクリーンショットを削除

--- spec/support/login_spec_helper.rb
+++ spec/support/login_spec_helper.rb
@@ -7,7 +7,6 @@ module LoginSpecHelper
 
   def login_as(user)
     visit login_path
-    page.save_screenshot "capybara.png"
     fill_in 'email', with: user.email
     fill_in 'password', with: 'password'
     find('#login-btn').click
