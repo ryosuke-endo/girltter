c1741ebe71d2e32434dc3af9fe63277fef0e509d
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-10-23 14:08:40 +0900

    テストを追加

--- spec/features/comment_spec.rb
+++ spec/features/comment_spec.rb
@@ -1,15 +1,19 @@
 require 'rails_helper'
 
-describe 'comment', js: true do
-  let!(:member) { create(:member) }
+RSpec.feature 'comment', js: true do
+  let(:member) { create(:member) }
+  let(:love) { create(:love, :with_category) }
   before do
     login_as(member)
   end
 
   context '投稿' do
     context '成功' do
-      it 'modalが表示される' do
-        visit '/'
+      it 'コメント本文が表示される' do
+        visit love_path(love)
+        find('#comment_body').set('出直し')
+        find('#comment-btn').click
+        expect(page).to have_selector 'p', text: '出直し'
       end
     end
   end
