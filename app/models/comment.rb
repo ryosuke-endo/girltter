aba41896b2bbf4938264b87af5a0f8c603f1035d
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-11-10 16:57:53 +0900

    formのparamsの値を設定する

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :commentable, polymorphic: true
   belongs_to :member
+
   validates :body, presence: true
+
+  accepts_nested_attributes_for :commentable
 
   scope :answer_ids, -> { pluck(:commentable_id).uniq }
 end
