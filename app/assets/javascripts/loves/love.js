a615444fad9dd0ff0aedced7838c53b55cc90490
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-11-13 23:57:15 +0900

    不要なfunctionを削除

--- app/assets/javascripts/loves/love.js
+++ app/assets/javascripts/loves/love.js
@@ -1,12 +1,10 @@
 import Comment from '../comment'
 
-$(() => {
-  $("[data-form-submit-comment]").on('click', () => {
-    const text = document.querySelector("[data-form-text-area-comment]").value
-    if (text) {
-      const comment = new Comment();
-      event.preventDefault();
-      comment.send();
-    }
-  });
-})
+$("[data-form-submit-comment]").on('click', () => {
+  const text = document.querySelector("[data-form-text-area-comment]").value
+  if (text) {
+    const comment = new Comment();
+    event.preventDefault();
+    comment.send();
+  }
+});
