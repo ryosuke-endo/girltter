e02a7fd7ae15e8f26d3da0cb4a68045b8c4b74c7
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-11-20 10:29:58 +0900

    リファクタリング

--- app/models/love.rb
+++ app/models/love.rb
@@ -2,10 +2,12 @@ class Love < ActiveRecord::Base
   acts_as_taggable
   belongs_to :member
   belongs_to :category
+
   has_many :supplementals, as: :supplementable, dependent: :destroy
   has_many :comments, as: :commentable, dependent: :destroy
 
   before_validation :set_tags
+
   validates :body, presence: true
   validates :category_id, presence: true
   validates :title, presence: true
@@ -15,29 +17,27 @@ class Love < ActiveRecord::Base
     RedisService.count_up(id, name)
   end
 
-  class << self
-    def update_tags!
-      tags = ActsAsTaggableOn::Tag.pluck(:name)
-      ActiveRecord::Base.transaction do
-        Love.find_each do |love|
-          tags.each do |tag|
-            if !!(love.body.match(tag))
-              love.tag_list << tag
-            end
-          end
-          if love.tag_list.present?
-            love.save!
+  def self.update_tags!
+    ActiveRecord::Base.transaction do
+      Love.find_each do |love|
+        new.all_tags.each do |tag|
+          if !!(love.body.match(tag))
+            love.tag_list << tag
           end
         end
+        love.save! if love.tag_list.present?
       end
     end
+  end
+
+  def all_tags
+    ActsAsTaggableOn::Tag.pluck(:name)
   end
 
   private
 
   def set_tags
-    tags = ActsAsTaggableOn::Tag.pluck(:name)
-    tags.each do |tag|
+    all_tags.each do |tag|
       if !!(body.match(tag))
         tag_list << tag
       end
