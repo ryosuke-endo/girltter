97f9035776a4be88cd87bdbad3b82b708bb1eb7b
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-12-03 22:50:55 +0900

    テスト修正

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :category do
     name "恋愛相談"
     description "恋愛に関する相談を行う場所です"
-    type "LoveCategory"
+    image File.new("#{Rails.root}/spec/fixtures/image/1.jpg")
   end
 end
