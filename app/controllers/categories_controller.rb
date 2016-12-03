a8356648d2bbbad483d64bc29b5fa27246302795
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-12-03 14:08:18 +0900

    controllerのメソッドをsetに変更 不要なメソッドを削除

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,30 +1,21 @@
 class CategoriesController < ApplicationController
   skip_before_action :require_login
 
+  before_action :set_category
   before_action :set_tag_ranking
-  before_action :set_ranking
 
   def show
-    @category = Category.find(params[:id])
-    @categories = @category.class.all
-    @threads =
-      case @category
-      when LoveCategory
-        @category.loves.includes(:category, :tags, :member)
-          .order(created_at: :desc).page(params[:page])
-      end
   end
 
   private
 
+  def set_category
+    @category = Category.find(params[:id])
+    @categories = Category.all
+  end
+
   def set_tag_ranking
     ranking_ids = Tagging.ranking_ids("Love", 20)
     @tag_ranking = ActsAsTaggableOn::Tag.find(ranking_ids)
-  end
-
-  def set_ranking
-    @rankings = Ranking.where(start_date: DateTime.yesterday).
-      order(read_count: :desc).
-      limit(5)
   end
 end
