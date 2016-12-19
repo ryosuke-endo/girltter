f95ed9ebc1909403e6e9bdfa6887bb56669e31b4
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-12-19 22:53:53 +0900

    temp_fileのロジックを変更

---
+++ app/models/concerns/temp_fileable.rb
@@ -0,0 +1,25 @@
+module TempFileable
+  extend ActiveSupport::Concern
+  attr_reader :temp_file
+
+  def temp_file=(temp_file)
+    @temp_file = TempFile.new
+    temp_files = TempFile.where(temp_file_name: temp_file.original_filename)
+    if temp_files.count.zero?
+      @temp_file.temp = temp_file
+      @temp_file.save
+    else
+      sizes = temp_files.map(&:temp_file_size).select { |x| x == temp_file.size }
+      if sizes.blank?
+        @temp_file.temp = temp_file
+        @temp_file.save
+      else
+        @temp_file = temp_files.first
+      end
+    end
+  end
+
+  def temp_file_id
+    temp_file.id
+  end
+end
--- spec/factories/temp_files.rb
+++ spec/factories/temp_files.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
   factory :temp_file do
-    
+    temp File.new("#{Rails.root}/spec/fixtures/image/1.jpg")
   end
 end
