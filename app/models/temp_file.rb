0b67eb8c31631c2fb356b54cdb4e64eb57634556
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-12-23 09:18:16 +0900

    書き方を修正

--- app/models/temp_file.rb
+++ app/models/temp_file.rb
@@ -1,8 +1,8 @@
 class TempFile < ActiveRecord::Base
   has_attached_file :temp, styles: { medium: '300x300>', thumb: '140x140>' }
-  validates_attachment :temp,
-                       content_type: { content_type: ['image/jpg',
-                                                      'image/jpeg',
-                                                      'image/png',
-                                                      'image/gif'] }
+  validates_attachment_content_type :temp,
+                                    content_type: ['image/jpg',
+                                                   'image/jpeg',
+                                                   'image/png',
+                                                   'image/gif']
 end
