7e9707cae2cf43a741bf466dfd0ea7b79fed90a0
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-12-24 17:56:21 +0900

    新しい形式を追加

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,5 +1,7 @@
 Time::DATE_FORMATS[:YmdnHM] = -> (time) {
   date = time.to_date
   week = %w(日 月 火 水 木 金 土 日)[date.wday]
-  time.strftime("%Y/%m/%d（#{week}）%H:%M")
+  time.strftime("%Y/%m/%d(#{week})%H:%M")
 }
+
+Time::DATE_FORMATS[:Ymd] = '%Y/%m/%d'
