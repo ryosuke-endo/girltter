0f3da874376b22562877efb71f068c43057ef2bc
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2017-01-15 22:43:11 +0900

    テストを修正する

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,3 @@
 class Comment < ActiveRecord::Base
-  belongs_to :commentable, polymorphic: true
-  belongs_to :member
-
   validates :body, presence: true
-
-  scope :answer_ids, -> { pluck(:commentable_id).uniq }
 end
--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,8 +1,5 @@
 FactoryGirl.define do
   factory :comment do
     body "MyText"
-    member_id 1
-    commentable_id 1
-    commentable_type "Love"
   end
 end
