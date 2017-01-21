13d0093d2174df2231f31fd1c701607d06a400d0
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2017-01-21 15:11:16 +0900

    commentのimageにvalidationを追加する

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,17 @@
 class Comment < ActiveRecord::Base
+  has_attached_file :image,
+    styles: { normal: '500x500>',
+              thumbnail: '140x140>' }
+
   belongs_to :topic
 
   validates :body, presence: true
   validates :name, presence: true
+  validates_attachment :image,
+    content_type: { content_type: ['image/jpg',
+                                   'image/jpeg',
+                                   'image/png',
+                                   'image/gif',
+                                   'image/bmp'] },
+    less_than: 20.megabytes
 end
