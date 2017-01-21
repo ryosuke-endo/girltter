0aea58e21ced101aa9960aa2c95773497acb6c17
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2017-01-21 22:04:20 +0900

    scroll移動を追加する

--- app/assets/javascripts/topic.js
+++ app/assets/javascripts/topic.js
@@ -51,10 +51,13 @@ $('[data-reply-id]').on('click', function() {
   const reply_text = `>>${id}`;
   const $inputText = $('textarea')
   const $text = $inputText.val()
+  const $position = $('label[for="comment_body"]').offset()
 
   if($text.length === 0) {
     $inputText.val(reply_text);
   } else {
     $inputText.val(`${reply_text}\n${$text}`);
   }
+
+  window.scroll($position.left, $position.top);
 })
