255a50f5098c02f99f39928e10e1d651484148db
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2017-01-22 17:48:07 +0900

    loveをcontroller/model/viewから削除する

--- app/decorators/love_decorator.rb
+++
@@ -1,6 +0,0 @@
-# coding: utf-8
-module LoveDecorator
-  def short_body
-    body.length <= 100 ? body : truncate(body, length: 100, omission: '...')
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170122083546) do
+ActiveRecord::Schema.define(version: 20170122084647) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",               limit: 255,             null: false
@@ -38,19 +38,6 @@ ActiveRecord::Schema.define(version: 20170122083546) do
   end
 
   add_index "comments", ["topic_id"], name: "index_comments_on_topic_id", using: :btree
-
-  create_table "loves", force: :cascade do |t|
-    t.string   "title",       limit: 255,                    null: false
-    t.text     "body",        limit: 4294967295,             null: false
-    t.integer  "member_id",   limit: 4,                      null: false
-    t.integer  "category_id", limit: 4,                      null: false
-    t.integer  "read_count",  limit: 4,          default: 0, null: false
-    t.datetime "created_at",                                 null: false
-    t.datetime "updated_at",                                 null: false
-  end
-
-  add_index "loves", ["category_id"], name: "index_loves_on_category_id", using: :btree
-  add_index "loves", ["member_id"], name: "index_loves_on_member_id", using: :btree
 
   create_table "rankings", force: :cascade do |t|
     t.integer  "rankable_id",   limit: 4,               null: false
