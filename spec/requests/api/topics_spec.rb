c42b0525d2438d6e3fafcdfbf2be4ffc957ba0eb
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2017-01-29 23:00:37 +0900

    APIのテストを追加する

---
+++ spec/requests/api/topics_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'Api::TopicsController', type: :request do
+  describe '#create' do
+    context 'success' do
+      it 'response 200' do
+        params = { topic: FactoryGirl.build(:topic).attributes }
+        expect {
+          post api_topics_path(format: :json), params
+        }.to change { Topic.count }.by(1)
+        expect(response.status).to eq 200
+      end
+    end
+
+    context 'fail' do
+      it 'response 400' do
+        params = { topic: { title: "foo"} }
+        expect {
+          post api_topics_path(format: :json), params
+        }.to change { Topic.count }.by(0)
+        expect(response.status).to eq 400
+      end
+    end
+  end
+end
