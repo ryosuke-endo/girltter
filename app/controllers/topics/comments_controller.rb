f5e6e0be0fcccc143bd82129a2fd7d94da2a4f2d
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2017-02-09 22:06:12 +0900

    三項演算子で書く

--- app/controllers/topics/comments_controller.rb
+++ app/controllers/topics/comments_controller.rb
@@ -3,12 +3,7 @@ class Topics::CommentsController < Topics::ApplicationController
   def anchor
     topic = Topic.find(params[:topic_id])
     no = params[:no].to_i
-    anchor =
-      if no == 1
-        topic
-      else
-        topic.comments[no - 2]
-      end
+    anchor = no == 1 ? topic : topic.comments[no - 2]
     status = anchor.present? ? 200 : 404
     text = render_to_string partial: 'anchor',
                             locals: { no: no,
