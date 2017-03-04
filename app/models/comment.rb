b5c27a3af2c82a1caa951b3af8d21bdec9c42cf5
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2017-03-04 17:08:42 +0900

    add remove unnecessary association

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,8 +4,6 @@ class Comment < ActiveRecord::Base
               thumbnail: '140x140>' }
 
   belongs_to :topic
-  has_many :comment_reaction_icons
-  has_many :reaction_icons, through: :comment_reaction_icons
   has_many :reactions, as: :reactionable
   has_many :icons, through: :reactions
 
