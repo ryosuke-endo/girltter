cda60b43913787fc5835094f94181e690f4b2db8
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2017-03-05 14:23:34 +0900

    change component units

--- app/assets/javascripts/components/comment/reaction_counter.js
+++ app/assets/javascripts/components/comment/reaction_counter.js
@@ -5,19 +5,33 @@ axios.defaults.headers['X-CSRF-TOKEN'] = $('meta[name=csrf-token]').attr('conten
 
 export default Vue.extend({
   props: {
-    after: {
+    icons: {
       type: String
     },
     count: {
       type: String
     }
   },
+  data() {
+    return {
+      localIcons: JSON.parse(this.icons),
+      localCount: JSON.parse(this.count)
+    }
+  },
+  methods: {
+    spriteClass(icon) {
+      const hexName = icon.image_file_name.replace(/(unicode\/|\.png)/, '')
+      return `emoji-${hexName}`
+    }
+  },
   template: `
-  <div class="p-emoji__container c-flex c-border c-border-r-5">
-    <div :class="after">
-    </div>
-    <div class="p-emoji__counter">
-      {{count}}
+  <div class="c-container c-flex c-flex__wrap">
+    <div class="p-emoji__container c-flex c-border c-border-r-5" v-for="icon in localIcons">
+      <div :class="spriteClass(icon[0])">
+      </div>
+      <div class="p-emoji__counter">
+        {{localCount[icon[0].id]}}
+      </div>
     </div>
   </div>
   `
