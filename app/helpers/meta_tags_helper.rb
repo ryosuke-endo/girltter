d6b8ead4211bd2a3bc1423f0aed11dd6aa67e523
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2017-03-30 22:36:50 +0900

    refs #116 set meta_tags topic show

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -11,14 +11,23 @@ module MetaTagsHelper
                   keywords: keywords
   end
 
-  def page_title
-    [t('site_title')]
+  def topic_show_meta_tags(topic)
+    title = [topic.title, t('site_title')]
+    description = topic.body
+
+    set_meta_tags title: title,
+                  description: description
   end
 
   def top_meta_tags
-    options = {}
-    options[:title] = [t('site_title'), t("meta_tags.#{controller_name}.#{action_name}.title")]
-    options[:separator] = '-'
-    default_meta_tags(options)
+    title = [t('site_title'), t("meta_tags.#{controller_name}.#{action_name}.title")]
+    description = t("meta_tags.#{controller_name}.#{action_name}.description")
+    keywords = t("meta_tags.#{controller_name}.#{action_name}.keywords")
+    separator = '|'
+
+    set_meta_tags title: title,
+                  separator: separator,
+                  description: description,
+                  keywords: keywords
   end
 end
