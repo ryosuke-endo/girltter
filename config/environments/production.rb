8f57299c202d6899fb371f8e3de5aa5feb03607b
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2017-04-03 07:07:20 +0900

    refs #156 modify

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host:  'girltter.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: local_host,
+    address: 'localhost'
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
