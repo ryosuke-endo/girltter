5c73f8ba1f02db4f2a4583ccb663f1b4c7cb115e
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2017-04-02 23:49:38 +0900

    refs #156 add exception notification

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # exception notification
+  Rails.application.config.middleware.use ExceptionNotification::Rack,
+    email: {
+    deliver_with: :deliver, # Rails >= 4.2.1 do not need this option since it defaults to :deliver_now
+    email_prefix: 'error',
+    sender_address: %{"notifier" <notifier@griltter.com>},
+    exception_recipients: %w{girltterjp@gmail.com}
+  }
 end
