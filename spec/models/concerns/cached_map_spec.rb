f3e1d4db9e1064fd8bd899ecd1b1775183a012a8
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2017-04-04 22:22:14 +0900

    refs #172 fix test

--- spec/models/concerns/cached_map_spec.rb
+++ spec/models/concerns/cached_map_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe CachedMap, type: :model do
-  let(:category) { create(:category) }
+  let(:category) { create(:icon_category) }
 
   describe '#cached' do
     it '結果をhashで返す' do
       result = [[category.id, category]].to_h
-      expect(Category.cached).to eq result
+      expect(IconCategory.cached).to eq result
     end
   end
 
   describe '#find_cached' do
     it 'modelの中身を返す' do
-      expect(Category.find_cached(category.id)).to eq category
+      expect(IconCategory.find_cached(category.id)).to eq category
     end
   end
 end
