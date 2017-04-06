62395ac7b04e2cb91d894ba7801829340eeb1a57
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2017-04-06 22:20:08 +0900

    refs #182
    delete unused method

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,9 +1,2 @@
 class Tagging < ActsAsTaggableOn::Tagging
-  scope :ranking_ids, lambda { |type, count|
-    where(taggable_type: type)
-      .group(:tag_id)
-      .order('count(tag_id) desc')
-      .limit(count)
-      .pluck(:tag_id)
-  }
 end
