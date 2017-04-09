73b3ca669132d2bacd6bfd59f2cb58561162b8e4
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2017-04-09 14:40:37 +0900

    refs #186
    fileuploadを作成する

--- app/javascript/packs/components/sp/file_upload.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue/dist/vue'
-
-const CONTENT_TYPE = [
-  'image/jpg',
-  'image/jpeg',
-  'image/png',
-  'image/gif',
-  'image/bmp',
-]
--- app/javascript/packs/sp/topic.js
+++ app/javascript/packs/sp/topic.js
@@ -1,3 +1,89 @@
-console.log("foo")
-console.log("bar")
-console.log("bar")
+import Vue from 'vue/dist/vue'
+import axios from 'axios/dist/axios'
+
+import modalMixins from '../mixins/modal.js'
+
+import formError from '../components/common/form/error.js'
+import fileUpload from '../components/sp/topic/file_upload.js'
+import icon from '../components/topic/icon.js'
+import modal from '../components/common/form/modal.js'
+
+axios.defaults.headers['X-CSRF-TOKEN'] = $('meta[name=csrf-token]').attr('content')
+
+$(function() {
+  const topicForm = Vue.extend({
+    mixins: [modalMixins],
+    data() {
+      return {
+        categories: [],
+        topic: {
+          title: '',
+          body: '',
+          category_id: '',
+          name: '匿子さん',
+          image: '',
+          errors: {
+            count: 0,
+            keys: [],
+            messages: []
+          }
+        },
+      }
+    },
+
+    created() {
+      self = this
+      axios.get('/api/categories.json')
+      .then(function(res) {
+        self.categories = res.data
+        self.getCategoryId()
+      })
+    },
+    components: {
+      'form-error': formError,
+      'file-upload': fileUpload,
+      'icon': icon,
+      'modal': modal
+    },
+
+    methods: {
+      getCategoryId() {
+        const id = parseInt(location.href.match(/\d$/).join(''))
+        return this.topic.category_id = id
+      },
+      addUrl(url) {
+        const text = this.topic.body
+        if(text.length === 0) {
+          this.topic.body = url
+        } else {
+          this.topic.body = (`${text}\n\n${url}`)
+        }
+      },
+      submit() {
+        self = this
+        const topic_params = {
+          topic: self.topic
+        }
+        axios({
+          method: "post",
+          url: "/api/topics",
+          data: topic_params
+        })
+        .then(function(res) {
+          location.href = `/topics/complete?id=${res.data.id}`
+        })
+        .catch(function(error) {
+          self.topic.errors.count = parseInt(Object.keys(error.response.data.errors).length)
+          self.topic.errors.keys = error.response.data.errors
+          self.topic.errors.messages = error.response.data.error_messages
+        })
+      },
+      isError(name) {
+        if (this.topic.errors.keys[name]) {
+          return "field_with_errors"
+        }
+      }
+    }
+  })
+  new topicForm().$mount('#topic-form-vue')
+});
