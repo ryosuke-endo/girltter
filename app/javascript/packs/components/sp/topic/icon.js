3dccb7d1f22710ffe293cc85e56c4b63c2327845
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2017-04-09 17:25:39 +0900

    refs #189
    不要なメソッドを削除する

--- app/javascript/packs/components/sp/topic/icon.js
+++ app/javascript/packs/components/sp/topic/icon.js
@@ -10,7 +10,7 @@ export default Vue.extend({
   },
   template: `
     <div class="c-icon__items c-margin-l-10">
-      <i data="icon-item" class="fa fa-link c-icon-d-gray c-margin-r-10 text--s-lg c-icon__item" @click="showModal" @mouseenter="showLink" @mouseleave="hiddenLink">
+      <i data="icon-item" class="fa fa-link c-icon-d-gray c-margin-r-10 text--s-lg c-icon__item" @click="showModal">
       </i>
     </div>`
 })
