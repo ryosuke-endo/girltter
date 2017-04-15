a1504ca3fea23e91751aeb82958c200ab3308ef2
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2017-04-15 23:42:43 +0900

    refs #187
    共通の処理のファイルの置き場を修正する

--- app/javascript/packs/sp/comment.js
+++ app/javascript/packs/sp/comment.js
@@ -5,8 +5,8 @@ import axios from 'axios/dist/axios'
 import modalMixins from '../mixins/modal.js'
 
 import formError from '../components/common/form/error.js'
-import fileUpload from '../components/sp/topic/file_upload.js'
-import icon from '../components/sp/topic/icon.js'
+import fileUpload from '../components/common/sp/file_upload.js'
+import icon from '../components/common/sp/icon.js'
 import modal from '../components/common/form/modal.js'
 
 axios.defaults.headers['X-CSRF-TOKEN'] = $('meta[name=csrf-token]').attr('content')
