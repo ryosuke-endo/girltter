413166ff46cb9e07f58d5c14997c38c4ccdd2c29
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2017-04-18 22:29:25 +0900

    refs #119
    breadcrumbsの変数を変更する

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -14,28 +14,8 @@ crumb :tag do |tag|
   link "#{tag.name}"
 end
 
-crumb :thread do |thread|
-  category = thread.category
+crumb :topic do |topic|
+  category = topic.category
   link "#{category.name}", category_path(category.id)
-  link "#{thread.title}"
+  link "#{topic.title}"
 end
-# crumb :project do |project|
-#   link project.name, project_path(project)
-#   parent :projects
-# end
-
-# crumb :project_issues do |project|
-#   link "Issues", project_issues_path(project)
-#   parent :project, project
-# end
-
-# crumb :issue do |issue|
-#   link issue.title, issue_path(issue)
-#   parent :project_issues, issue.project
-# end
-
-# If you want to split your breadcrumbs configuration over multiple files, you
-# can create a folder named `config/breadcrumbs` and put your configuration
-# files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
-# folder are loaded and reloaded automatically when you change them, just like
-# this file (`config/breadcrumbs.rb`).
