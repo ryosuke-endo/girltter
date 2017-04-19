b61d36d7dd8b7d4e29273c1887fe25073c591d61
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2017-04-19 21:58:20 +0900

    refs #217
    ogの設定をする

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -3,21 +3,45 @@ module MetaTagsHelper
     title = [t("meta_tags.#{controller_name}.#{action_name}.title"), t('site_name')]
     description = t("meta_tags.#{controller_name}.#{action_name}.description")
     keywords = t("meta_tags.#{controller_name}.#{action_name}.keywords")
+    icon = opts[:icon] || '/favicon.ico'
+    type = opts[:type] || 'article'
+    url = opts[:url] || request.original_url
+    image = opts[:image_url] || image_url('og.png')
 
-    set_meta_tags(title: title,
-                  description: description,
-                  keywords: keywords,
-                  reverse: opts[:reverse])
+    configs = {
+      title: title,
+      description: description,
+      keywords: keywords,
+      reverse: opts[:reverse],
+      icon: icon,
+      og: {
+        title: title.join(' | '),
+        site_name: t('site_name'),
+        description: description,
+        type: type,
+        url: url,
+        image: image
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@girltter_',
+        title: title.join(' | '),
+        description: description,
+        image: image
+      }
+    }
+
+    set_meta_tags(configs)
   end
 
-    def category_meta_tags(category)
+  def category_meta_tags(category)
     title = ["「#{category.name}」に関するトピック", t('site_name')]
     description = category.description
     keywords = [category.name]
 
     set_meta_tags title: title,
-                  description: description,
-                  keywords: keywords
+      description: description,
+      keywords: keywords
   end
 
   def tags_meta_tags(tag)
